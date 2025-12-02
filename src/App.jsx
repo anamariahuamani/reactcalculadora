28f50c2c5812c4a73479a64a6b3f7036976e6896
https://github.com/anamariahuamani/mi-primera-app.git <huamaniayquipaanamaria30@gmail.com>
2025-12-02 11:55:46 -0500

    subiendo el reactcalculadora

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Calculadora from './componentes/Calculadora'
+
+function App() {
+  return (
+    <div>
+      <Calculadora/>
+    </div>
+  )
+}
+
+export default App
